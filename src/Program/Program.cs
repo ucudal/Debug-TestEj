7457c6a62a605e6a2e2e199900c702237321a6f2
alejandropiccardo <alejandro.piccardo@correo.ucu.edu.uy>
2022-09-05 10:26:34 -0300

    Corregido diferencia de espacios

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -19,14 +19,14 @@ namespace WordsPhrases
         {
             const string text1 = " text1 ";
             const string text2 = " text2 ";
-            const string expected = "text1 text2";
+            const string expected = "  text1   text2 ";
             Phrase phrase = new Phrase();
             Word word1 = new Word(text1);
             Word word2 = new Word(text2);
             phrase.AddWord(word1);
             phrase.AddWord(word2);
             string actual = phrase.GetPhrase();
-            //Assert.AreEqual(expected, actual);
+           // Assert.AreEqual(expected, actual);
 
             if (actual == expected)
             {
